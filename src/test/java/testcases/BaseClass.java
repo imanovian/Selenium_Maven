9bf86849c001689c1cc72347b5c5c83a62a03ddb
Your Name <you@example.com>
2022-09-18 12:14:37 +0530

    update

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -2,6 +2,7 @@ package testcases;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -13,11 +14,16 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 public class BaseClass {
 	
 	WebDriver driver;
 	XSSFWorkbook wbook;
     XSSFSheet sheet;
+    ExtentReports report;
+    ExtentTest test;
     
     @BeforeTest
     public void DataSetup() throws IOException {
@@ -25,14 +31,21 @@ public class BaseClass {
         
         wbook = new XSSFWorkbook(fis);
         sheet = wbook.getSheet("Sheet1");
+        
+        
+        
+        report = new ExtentReports("ExtentReport.html");
     }
     @AfterTest
     public void DataClean() throws IOException {
-    	
+    	report.flush();
+    	report.close();
+    
     	//wbook.close();
     }
 	@BeforeMethod 
-	public void Setup() {
+	public void Setup(Method method) {
+		test = report.startTest(method.getName());
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.simplilearn.com/");
@@ -41,6 +54,7 @@ public class BaseClass {
 	}
 	@AfterMethod 
 	public void TearDown() {
+		report.endTest(test);
 		driver.close();
 	}
 }
--- src/test/java/testcases/SimplilearnLoginTest.java
+++ src/test/java/testcases/SimplilearnLoginTest.java
@@ -11,6 +11,8 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 import pages.LoginPage;
 
 public class SimplilearnLoginTest extends BaseClass{
@@ -18,7 +20,7 @@ public class SimplilearnLoginTest extends BaseClass{
 	
 	@Test
 	public void Test1() {
-		
+		test.log(LogStatus.INFO, "Test1 Started");
 		LoginPage lp = new LoginPage(driver);
 		lp.Login("xyz@abc.com","Test@123");
 		
@@ -39,11 +41,13 @@ public class SimplilearnLoginTest extends BaseClass{
 	@Test
 	@Parameters({"uname","pwd"})
 	public void Test2(String UserName, String Password) {
+		test.log(LogStatus.INFO, "Test2 Started");
 		LoginPage lp = new LoginPage(driver);
 		lp.Login(UserName,Password);
 	}
 	@Test
 	public void Test3() {
+		test.log(LogStatus.INFO, "Test3 Started");
 		String UserName = sheet.getRow(1).getCell(0).getStringCellValue();
         String Password = sheet.getRow(1).getCell(1).getStringCellValue();
         LoginPage lp = new LoginPage(driver);
